69bbac84480cf9ce7908b640a22871d450b8e27b
Tavilo Breno <breno_wk2@hotmail.com>
2026-04-18 02:26:51 -0300

    Add MenuItem entity and IMenuService interface with methods for menu retrieval

---
+++ GoodHamburger.Core/Entities/MenuItem.cs
@@ -0,0 +1,19 @@
+﻿namespace GoodHamburger.Core.Entities;
+
+public class MenuItem
+{
+    public string Id { get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public MenuCategory Category { get; set; }
+    public bool IsActive { get; set; } = true;
+    public int DisplayOrder { get; set; }
+}
+
+public enum MenuCategory
+{
+    Sandwich,
+    SideDish,
+    Drink
+}
---
+++ GoodHamburger.Core/Interfaces/IMenuService.cs
@@ -0,0 +1,20 @@
+﻿using GoodHamburger.Core.Entities;
+
+namespace GoodHamburger.Core.Interfaces;
+
+public interface IMenuService
+{
+    Task<IEnumerable<MenuItem>> GetSandwichesAsync();
+    Task<IEnumerable<MenuItem>> GetSideDishesAsync();
+    Task<IEnumerable<MenuItem>> GetDrinksAsync();
+    Task<MenuItem?> GetMenuItemByIdAsync(string id);
+    Task<DiscountRule[]> GetDiscountRulesAsync();
+}
+
+public record DiscountRule
+{
+    public string Name { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public string Discount { get; init; } = string.Empty;
+    public string Condition { get; init; } = string.Empty;
+}
